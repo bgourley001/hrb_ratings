f3e0eb4503a3dc91735f85fa023bf24768b8227c
Bill Gourley <bgourley001@users.noreply.github.com>
2022-11-14 20:01:31 +0000

    Added csv files for 2022-11-14

---
+++ hrbRatings/get_csv_from_downloads.py
@@ -0,0 +1,30 @@
+import os
+import datetime
+
+downloads_path = 'C:\\Users\\bgour\\Downloads\\'
+dest_path = 'G:\\Dev2022\\projects\\hrb_ratings\\hrbRatings\\csv_downloads\\'
+
+current_date = datetime.date.today()
+
+# copy files from Downloads to csv_downloads sub-folders
+file_names = ['cards_','dailyreport-', 'formreport_', 'jockeysreport-', 'LastTenDistances_report_', 
+					'OR_report_', 'trainersreport-']
+file_categories = ['cards','daily_reports','form_reports','jockey_reports','last10_reports','or_reports','trainer_reports']
+
+#count = 0
+#for file_category in file_categories:
+#	folder = file_category + '\\'
+#	file_name = file_names[count]
+#	filename =  downloads_path + file_name + str(current_date) + '*'
+#	cmd = 'move ' + filename + ' ' + dest_path + folder
+#	print('cmd = ' + cmd)
+#	os.system(cmd)
+#	count += 1
+
+cmd = 'git add .'
+os.system(cmd)
+cmd = 'git commit -m "Added csv files for "' + str(current_date)
+os.system(cmd)
+cmd = 'git push origin main'
+os.system(cmd)
+
