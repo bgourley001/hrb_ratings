8434da681d8ac29c269605f18d97b5f0ece2b917
Bill Gourley <bgourley001@users.noreply.github.com>
2022-12-14 11:15:06 +0000

    Added csv files for 2022-12-14

--- hrbRatings/temp.py
+++ hrbRatings/temp.py
@@ -1,22 +1,6 @@
 import numpy as np
 
-a = np.array([[2,1], [1,3]])
+x = np.random.rand(4,5)
+y =np.sum(x,axis=1)
 
-print(np.dot(a,a))
-
-print(np.log(0.5))
-
-a = np.random.randn(3,4)
-b = np.random.randn(1,4)
-
-c = a + b
-
-print(f'c = {c}')
-print(f'c.shape = {c.shape}')
-
-a = np.random.randn(1,3)
-b = np.random.randn(3,3)
-c = a*b
-
-print(f'c = {c}')
-print(f'c.shape = {c.shape}')
+print(y.shape)
