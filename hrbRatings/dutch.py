523c6b8a9db13c1df54467a03d1738c3f4f8de6f
Bill Gourley <bgourley001@users.noreply.github.com>
2023-01-28 09:50:03 +0000

    Added csv files for 2023-01-28

--- hrbRatings/dutch.py
+++ hrbRatings/dutch.py
@@ -5,9 +5,12 @@ def dutching_calculator(odds1, odds2, odds3, stake=5.0):
     stake3 = round((stake * (1 / odds3) / total_odds),2)
     return stake1, stake2, stake3
 
-odds1 = (4/1)+1
-odds2 = (20/1)+1
-odds3 = (13/2)+1
+odds1 = (10/3)+1
+odds2 = (6/1)+1
+odds3 = (4/1)+1
+#odds1 = 5.2
+#odds2 = 7.8
+#odds3 = 8.2
 stake = 5.0
 
 stake1, stake2, stake3 = dutching_calculator(odds1, odds2, odds3, stake)
