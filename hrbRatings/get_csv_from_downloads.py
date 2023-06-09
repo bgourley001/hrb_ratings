4a59039380a15abbe05efc6f8eb5897e8ba926bf
Bill Gourley <bgourley001@gmail.com>
2023-06-09 19:34:41 +0100

    added new version of get_csv files

--- hrbRatings/get_csv_from_downloads.py
+++ hrbRatings/get_csv_from_downloads.py
@@ -1,36 +1,59 @@
 import os
+import glob
 import datetime
 
-# set up paths
-downloads_path = 'C:\\Users\\bgour\\Downloads\\'
-dest_path = 'G:\\Dev2022\\projects\\hrb_ratings\\hrbRatings\\csv_downloads\\'
+def get_download_date():
+	current_date = datetime.date.today()
+	# current_date = datetime.date.today() + datetime.timedelta(days=2)
 
-current_date = datetime.date.today()
-# current_date = datetime.date.today() + datetime.timedelta(days=2)
+	return current_date
 
-# copy files from Downloads to csv_downloads sub-folders
-file_names = ['cards_','dailyreport-', 'formreport_', 'jockeysreport-', 'LastTenDistances_report_', 
-					'OR_report_', 'trainersreport-', 'GoingReport_', 'Weight_report_', 'GradeReport_', 'CourseDist_']
-file_categories = ['cards','daily_reports','form_reports','jockey_reports','last10_reports','or_reports','trainer_reports',
-		   'going_reports','weight_reports','grade_reports','course_dist']
+def set_file_paths():
+	if os.name == 'nt':  # Windows
+		downloads_path = os.path.join('C:/', 'Users/bgour/Downloads/')
+		dest_path = os.path.join('G:/', 'Dev2022/projects/hrb_ratings/hrbRatings/csv_downloads/temp2/')
+	else:  # Linux or macOS
+		downloads_path = os.path.join('/home', 'bill/Downloads/')
+		dest_path = os.path.join('/home','bill/development/python_code/hrb_ratings/hrbRatings/csv_downloads/temp/')
+	
+	#print(f'downloads_path : {downloads_path}')
+    
+	return downloads_path, dest_path
 
-count = 0
-for file_category in file_categories:
-	folder = file_category + '\\'
-	file_name = file_names[count]
+def copy_to_dest():
+	# copy files from Downloads to csv_downloads sub-folders
+	downloads_path, dest_path = set_file_paths()
+	file_names = ['cards_','dailyreport-', 'formreport_', 'jockeysreport-', 'LastTenDistances_report_', 
+			'OR_report_', 'trainersreport-', 'GoingReport_', 'Weight_report_', 'GradeReport_', 'CourseDist_']
+	file_categories = ['cards','daily_reports','form_reports','jockey_reports','last10_reports','or_reports','trainer_reports',
+		   'going_reports','weight_reports','grade_reports','course_dist_reports']
 
-	filename =  downloads_path + file_name + str(current_date) + '*'
+	count = 0
+	for file_category in file_categories:
+		folder = f'{file_category}/'
+		file_name = file_names[count]
+		for name in glob.glob(f'{downloads_path}{file_name}*.*'):
+			basename = os.path.basename(name)
+			print(f'name : {name}, {basename}')
+			os.rename(f'{name}', f'{dest_path}{folder}{basename}')
+		count += 1
 
-	cmd = 'move ' + filename + ' ' + dest_path + folder
-	print('cmd = ' + cmd)
-	os.system(cmd)
-	count += 1
+def main():
+	downloads_path, dest_path = set_file_paths()
+
+	copy_to_dest()   
+
+
 
 # update github
-cmd = 'git add .'
-os.system(cmd)
-cmd = 'git commit -m "Added csv files for "' + str(current_date)
-os.system(cmd)
-cmd = 'git push origin main'
-os.system(cmd)
+#cmd = 'git add .'
+#os.system(cmd)
+#cmd = 'git commit -m "Added csv files for "' + str(current_date)
+#os.system(cmd)
+#cmd = 'git push origin main'
+#os.system(cmd)
+
+if __name__ == "__main__":
+      main()
+      
 
